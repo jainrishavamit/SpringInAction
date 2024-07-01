bd09b8e53eb8314db56774179cd553180a33df3e
Jain Rishav Amit <jainrishavamit1@gmail.com>
2024-07-01 10:56:08 +0100

    Code cleanup and refactoring (#7)

--- src/main/java/com/rj/SpringInAction/controllers/MyStrorePublicController.java
+++ src/main/java/com/rj/SpringInAction/controllers/MyStrorePublicController.java
@@ -2,11 +2,9 @@ package com.rj.SpringInAction.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rj.SpringInAction.models.Cart;
-import com.rj.SpringInAction.models.LoginRequestDto;
-import com.rj.SpringInAction.models.LoginResponseDto;
 import com.rj.SpringInAction.models.Product;
-import com.rj.SpringInAction.models.User;
+import com.rj.SpringInAction.models.dto.LoginRequestDto;
+import com.rj.SpringInAction.models.dto.LoginResponseDto;
 import com.rj.SpringInAction.service.CartService;
 import com.rj.SpringInAction.service.LoginService;
 import com.rj.SpringInAction.service.ProductService;
@@ -15,15 +13,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
-public class MyStroreController {
+@RequestMapping("/api/public")
+public class MyStrorePublicController {
     
     @Autowired
     ProductService productService;
@@ -34,29 +32,18 @@ public class MyStroreController {
     @Autowired
     LoginService loginService;
 
-    @GetMapping("/api/public/product/search")
+    /*
+     * Remove requried=false from RequestParam, if need to make it required
+     * if done, 
+     */
+    @GetMapping("/product/search")
     public ResponseEntity<List<Product>> getProductyByKeyword(@RequestParam(required = false) String keyword) {
         return productService.searchProductByKeyword(keyword);
     }
 
-    @GetMapping("/api/public/getToken")
+    @GetMapping("/getToken")
     public ResponseEntity<LoginResponseDto> getMethodName(@RequestBody LoginRequestDto loginDetails) {
         return ResponseEntity.ok().body(loginService.login(loginDetails));
     }
-    
 
-    @GetMapping("/api/auth/consumer/cart")
-    public ResponseEntity<List<Cart>> getCartOfConsumer() {
-        String username = ((User)(SecurityContextHolder.getContext().getAuthentication().getPrincipal())).getUsername();
-        return cartService.getConsumerCart(username);
-    }
-
-    @GetMapping({"api/auth/seller/product/{id}","api/auth/seller/product","api/auth/seller/product/"})
-    public ResponseEntity<List<Product>> getMethodName(@PathVariable(required = false) Integer id) {
-        String username = ((User)(SecurityContextHolder.getContext().getAuthentication().getPrincipal())).getUsername();
-        return  productService.findProductByUsername(username,id);
-    }
-    
-    
-    
 }
