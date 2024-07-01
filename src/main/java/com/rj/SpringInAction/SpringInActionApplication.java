eff6ea24f774208f42903af32b66786e64bbcd85
Jain Rishav Amit <jainrishavamit1@gmail.com>
2024-07-01 05:28:05 +0100

    Enabling Swagger Docs (#5)

--- src/main/java/com/rj/SpringInAction/SpringInActionApplication.java
+++ src/main/java/com/rj/SpringInAction/SpringInActionApplication.java
@@ -3,7 +3,10 @@ package com.rj.SpringInAction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
 @SpringBootApplication
+@EnableSwagger2WebMvc
 public class SpringInActionApplication {
 
 	public static void main(String[] args) {
